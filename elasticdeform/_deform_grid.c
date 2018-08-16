7b2197bfdffef18743850adfa92e9df58021d1b6
Gijs van Tulder <gvtulder@gmail.com>
2018-08-16 21:09:35 +0200

    Check and convert inputs if necessary.

--- elasticdeform/_deform_grid.c
+++ elasticdeform/_deform_grid.c
@@ -62,7 +62,9 @@ static PyObject *Py_DeformGrid(PyObject *obj, PyObject *args)
 {
     PyObject *inputList = NULL, *outputList = NULL;
     PyArrayObject **inputs = NULL, **outputs = NULL;
-    PyArrayObject *displacement = NULL, *output_offset = NULL;
+    PyArrayObject *displacement = NULL, *outputOffset = NULL;
+    PyArrayObject *outputOffsetIn = NULL;
+    PyArrayObject *ordersIn = NULL, *modesIn = NULL, *cvalsIn = NULL;
     PyArrayObject *orders = NULL, *modes = NULL, *cvals = NULL;
     int *order = NULL, *mode = NULL;
     double *cval = NULL;
@@ -72,41 +74,97 @@ static PyObject *Py_DeformGrid(PyObject *obj, PyObject *args)
     if (!PyArg_ParseTuple(args, "O!O&O&O!O&O&O&",
                           &PyList_Type, &inputList,
                           NI_ObjectToInputArray, &displacement,
-                          NI_ObjectToOptionalInputArray, &output_offset,
+                          NI_ObjectToOptionalInputArray, &outputOffsetIn,
                           &PyList_Type, &outputList,
-                          NI_ObjectToInputArray, &orders,
-                          NI_ObjectToInputArray, &modes,
-                          NI_ObjectToInputArray, &cvals))
+                          NI_ObjectToInputArray, &ordersIn,
+                          NI_ObjectToInputArray, &modesIn,
+                          NI_ObjectToInputArray, &cvalsIn))
         goto exit;
 
+    /* check and collect inputs, outputs from lists */
     ninputs = PyList_Size(inputList);
-    // TODO check size of outputlist
-    // TODO check malloc
-    // TODO check size of inputs, outputs
+    if (ninputs == 0 || ninputs != PyList_Size(outputList)) {
+        PyErr_SetString(PyExc_RuntimeError, "invalid number of inputs/outputs");
+        goto exit;
+    }
     inputs = malloc(ninputs * sizeof(PyArrayObject*));
     outputs = malloc(ninputs * sizeof(PyArrayObject*));
+    if (!inputs || !outputs) {
+        PyErr_NoMemory();
+        goto exit;
+    }
     for(i = 0; i < ninputs; i++) {
         if (!NI_ObjectToInputArray(PyList_GetItem(inputList, i), &inputs[i]))
             goto exit;
         if (!NI_ObjectToOutputArray(PyList_GetItem(outputList, i), &outputs[i]))
             goto exit;
+        if (PyArray_SIZE(inputs[i]) != PyArray_SIZE(inputs[0])) {
+          PyErr_SetString(PyExc_RuntimeError, "all inputs should have the same size");
+          goto exit;
+        }
+        if (PyArray_SIZE(outputs[i]) != PyArray_SIZE(outputs[0])) {
+          PyErr_SetString(PyExc_RuntimeError, "all outputs should have the same size");
+          goto exit;
+        }
     }
 
-    // TODO check size and type of orders, modes, cvals
-    // TODO check malloc
+    /* check shape of displacement */
+    if ((PyArray_NDIM(displacement) != PyArray_NDIM(inputs[0]) + 1) ||
+        (PyArray_DIM(displacement, 0) != PyArray_NDIM(inputs[0])) ||
+        (PyArray_SIZE(displacement) == 0)) {
+        PyErr_SetString(PyExc_RuntimeError, "invalid displacement shape");
+        goto exit;
+    }
+
+    /* convert type of orders, modes and cvals (if required) */
+    orders = (PyArrayObject*)PyArray_FromArray(ordersIn, PyArray_DescrFromType(NPY_INT64),
+                                               NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_ALIGNED);
+    modes = (PyArrayObject*)PyArray_FromArray(modesIn, PyArray_DescrFromType(NPY_INT64),
+                                              NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_ALIGNED);
+    if (!orders || !modes) {
+        PyErr_SetString(PyExc_RuntimeError, "order and mode must be of type int64");
+        goto exit;
+    }
+    cvals = (PyArrayObject*)PyArray_FromArray(cvalsIn, PyArray_DescrFromType(NPY_DOUBLE),
+                                              NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_ALIGNED);
+    if (!cvals) {
+        PyErr_SetString(PyExc_RuntimeError, "cval could not be converted to double");
+        goto exit;
+    }
+    if ((PyArray_SIZE(orders) != ninputs) ||
+        (PyArray_SIZE(modes) != ninputs) ||
+        (PyArray_SIZE(cvals) != ninputs)) {
+       PyErr_SetString(PyExc_RuntimeError, "number of orders, modes, cvals must match inputs");
+       goto exit;
+    }
     order = malloc(ninputs * sizeof(int));
     mode = malloc(ninputs * sizeof(int));
     cval = malloc(ninputs * sizeof(double));
+    if (!order || !mode || !cval) {
+        PyErr_NoMemory();
+        goto exit;
+    }
     for(i = 0; i < ninputs; i++) {
-        order[i] = *(int *)PyArray_GETPTR1(orders, i);
-        mode[i] = *(int *)PyArray_GETPTR1(modes, i);
-        cval[i] = *(double *)PyArray_GETPTR1(cvals, i);
+        order[i] = (int)(*(npy_int64 *)PyArray_GETPTR1(orders, i));
+        mode[i] = (int)(*(npy_int64 *)PyArray_GETPTR1(modes, i));
+        cval[i] = (double)(*(npy_double *)PyArray_GETPTR1(cvals, i));
     }
 
-    // TODO check output_offset type (should be npy_intp)
+    /* check and convert output offset, if given */
+    if (outputOffsetIn) {
+        if (PyArray_SIZE(outputOffsetIn) != PyArray_NDIM(inputs[0])) {
+            PyErr_SetString(PyExc_RuntimeError, "incorrect length for output offset");
+            goto exit;
+        }
+        outputOffset = (PyArrayObject*)PyArray_FromArray(outputOffsetIn,
+                                           PyArray_DescrFromType(NPY_INTP), 0);
+        if (!outputOffset) {
+            PyErr_SetString(PyExc_RuntimeError, "invalid output offset");
+            goto exit;
+        }
+    }
 
-    // TODO
-    DeformGrid(ninputs, inputs, displacement, output_offset,
+    DeformGrid(ninputs, inputs, displacement, outputOffset,
                outputs, order, mode, cval);
     #ifdef HAVE_WRITEBACKIFCOPY
         for(i = 0; i < ninputs; i++) {
@@ -116,7 +174,11 @@ static PyObject *Py_DeformGrid(PyObject *obj, PyObject *args)
 
 exit:
     Py_XDECREF(displacement);
-    Py_XDECREF(output_offset);
+    Py_XDECREF(outputOffsetIn);
+    Py_XDECREF(outputOffset);
+    Py_XDECREF(ordersIn);
+    Py_XDECREF(modesIn);
+    Py_XDECREF(cvalsIn);
     Py_XDECREF(orders);
     Py_XDECREF(modes);
     Py_XDECREF(cvals);
